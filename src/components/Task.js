82f275135e97ddded1c7c13b07ed862f9501527d
Lisa Utsett <lisabethutsett@gmail.com>
2023-01-30 16:16:02 -0500

    worked on waves 0-2

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { useState } from 'react';
 import './Task.css';
 
 const Task = ({ id, title, isComplete }) => {
+  const [completed, setIsComplete] = useState(isComplete);
+  const setCompleted = () => {
+    setIsComplete(!completed);
+  };
+
+  const completedClass = completed ? 'tasks__item__toggle--completed' : 'tasks__item__toggle';
   return (
     <li className="tasks__item">
-      <button className="tasks__item__toggle">{title}</button>
+      <button className={ completedClass } onClick={ setCompleted }>{ title }</button>
       <button className="tasks__item__remove button">x</button>
     </li>
   );
